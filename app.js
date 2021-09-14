64637dabf7d367a3336a529d11e989c85c729786
Ferdian9991 <ferdisatria15@gmail.com>
2021-09-15 00:45:13 +0700

    cookie parser

--- app.js
+++ app.js
@@ -25,6 +25,22 @@ app.use('/',(req,res)=>{
   })
 })
 
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404))
